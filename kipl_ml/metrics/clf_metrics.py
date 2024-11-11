f63bf8aaa3c0d37be6754e53f74004312568909e
topiko <topiko1987@gmail.com>
2024-11-11 15:56:33 +0100

    Metrics.

---
+++ kipl_ml/metrics/clf_metrics.py
@@ -0,0 +1,49 @@
+"""
+Classification metrics.
+"""
+
+import torch
+from torch import nn
+
+
+class Metric:
+    name: str
+    objective: str
+
+    def __call__(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> float:
+        raise NotImplementedError
+
+
+class CrossEntropyLoss(Metric):
+    name: str = "CrossEntropyLoss"
+    objective: str = "min"
+
+    def __init__(self, *args, **kwargs):
+        self.loss = nn.CrossEntropyLoss(*args, **kwargs)
+
+    def __call__(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> float:
+        return self.loss(y_pred, y_true).item()
+
+
+class Accuracy(Metric):
+    name: str = "accuracy"
+    objective: str = "max"
+
+    def __call__(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> float:
+        return (y_pred == y_true).float().mean().item()
+
+
+METRICS = {Accuracy, CrossEntropyLoss}
+
+
+def get_objective(metric_name: str) -> str:
+    for metric in METRICS:
+        if metric.name == metric_name:
+            return metric.objective
+    raise ValueError(f"Metric {metric_name} not found.")
+
+
+def evaluate(
+    metrics: list[Metric], y_pred: torch.Tensor, y_true: torch.Tensor
+) -> dict[str, float]:
+    return {metric.name: metric(y_pred, y_true) for metric in metrics}
