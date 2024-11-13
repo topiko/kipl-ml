ac8596fbc7d5d0c838eb9b59943448afd306cd45
topiko <topiko1987@gmail.com>
2024-11-13 11:23:20 +0100

    Tryingto get the VizTr to work.

--- experiment/laserbeak/main.py
+++ experiment/laserbeak/main.py
@@ -10,6 +10,7 @@ from kipl_ml.logging.logger import get_logger
 from kipl_ml.metrics.clf_metrics import Accuracy, CrossEntropyLoss
 from kipl_ml.train.loops import train_model
 from omegaconf import DictConfig
+from src.cls_cvt import ConvolutionalVisionTransformer
 from src.transdfnet import DFNet
 from torch import nn
 from torch.utils.data import DataLoader
@@ -36,15 +37,29 @@ class WrapDFNet(DFNet):
         return super().forward(x, sample_sizes, return_feats, *args, **kwargs)
 
 
+class CNNVisTransformer(ConvolutionalVisionTransformer):
+    name = "lasereak_cvt"
+
+    def forward(self, x: torch.Tensor):
+        x = torch.cat([x_.unsqueeze(1) for x_ in x.values()], dim=1)
+        return super().forward(x)
+
+
 def get_model(model_name: str, n_classes: int, inputs: dict[str, int]) -> nn.Module:
+
+    if len(set(inputs.values())) != 1:
+        raise ValueError("All inputs must have the same size.")
+
+    input_size = inputs[list(inputs.keys())[0]]
+
     if model_name == "dfnet":
-
-        if len(set(inputs.values())) != 1:
-            raise ValueError("All inputs must have the same size.")
-
-        input_size = inputs[list(inputs.keys())[0]]
-
-        return WrapDFNet(n_classes, len(inputs), input_size=input_size)
+        return WrapDFNet(
+            num_classes=n_classes, input_channels=len(inputs), input_size=input_size
+        )
+    if model_name == "cvt":
+        return CNNVisTransformer(
+            num_classes=n_classes, in_chans=len(inputs), input_size=input_size
+        )
 
     raise NotImplementedError("Model not implemented yet.")
 
