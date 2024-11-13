56a0e625f9fcc7418cba72a048072c3f1676a1ac
topiko <topiko1987@gmail.com>
2024-11-13 12:27:51 +0100

    More transforms, also compose ones.

--- kipl_ml/flow/transforms.py
+++ kipl_ml/flow/transforms.py
@@ -21,6 +21,12 @@ class _TR:
         self._input_size: int | None = None
 
     @property
+    def input_size(self) -> int:
+        if self._input_size is None:
+            raise ValueError("Input size not set. Need to call fit first.")
+        return self._input_size
+
+    @property
     def output_size(self) -> int:
         if self._output_size is None:
             raise ValueError("Output size not set. Need to call fit first.")
@@ -75,6 +81,92 @@ class UDPackets(_TR):
         return mask.float()
 
 
+class Normalize(_TR):
+    NAME = "normalized"
+
+    def get_shapes(self, flow: dict[str, torch.Tensor]) -> Normlaize:
+        self._input_size = flow[self.asset].shape[0]
+        self._output_size = flow[self.asset].shape[0]
+        return self
+
+    def __call__(self, flow: dict[str, torch.Tensor]) -> torch.Tensor:
+        if (std := flow[self.asset].std()) == 0:
+            if all(flow[self.asset] == 0):
+                return flow[self.asset]
+            raise ValueError("Standard deviation is zero. Cannot normalize.")
+        return (flow[self.asset] - flow[self.asset].mean()) / std
+
+
+class IAT(_TR):
+    NAME = "iat"
+
+    def __init__(self, dir_key: str):
+        if dir_key not in {"up", "down", "any"}:
+            raise ValueError("Dir key must be either 'up' or 'down', or 'any'")
+        self.dir_key = dir_key
+        super().__init__(assets.TIME)
+
+    @property
+    def name(self) -> str:
+        if self.dir_key == "any":
+            return self.NAME
+
+        return f"{self.dir_key}_{self.NAME}"
+
+    def get_shapes(self, flow: dict[str, torch.Tensor]) -> IAT:
+        self._input_size = flow[self.asset].shape[0]
+        self._output_size = flow[self.asset].shape[0]
+        return self
+
+    def __call__(self, flow: dict[str, torch.Tensor]) -> torch.Tensor:
+        iats = torch.zeros_like(flow[assets.TIME])
+
+        if self.dir_key == "up":
+            mask = flow[assets.DIR] > 1
+        elif self.dir_key == "down":
+            mask = flow[assets.DIR] < 1
+        else:
+            mask = torch.ones_like(flow[assets.DIR], dtype=torch.bool)
+
+        iats[mask][1:] = torch.diff(flow[assets.TIME][mask], dim=0)
+        return iats
+
+
+class Compose(_TR):
+    NAME = "compose"
+
+    def __init__(self, *transforms: _TR):
+        self.transforms = transforms
+        self._output_size = None
+        self._input_size = None
+
+    @property
+    def name(self) -> str:
+        return "pipe:" + "-->".join(tr.name for tr in self.transforms)
+
+    def get_shapes(self, flow: dict[str, torch.Tensor]) -> Compose:
+        outs = None
+        for i, tr in enumerate(self.transforms):
+            tr.get_shapes(flow)
+            outs = tr.output_size
+            flow[tr.name] = flow[tr.asset][:outs]
+
+            if i == 0:
+                self._input_size = tr.input_size
+
+        self._output_size = outs
+
+        return self
+
+    def __call__(self, flow: dict[str, torch.Tensor]) -> torch.Tensor:
+        for i, tr in enumerate(self.transforms):
+            tensor = tr(flow)
+            if i < len(self.transforms) - 1:
+                flow[self.transforms[i].name] = tensor
+
+        return tensor
+
+
 class FeatureTrs:
     def __init__(
         self,
@@ -106,9 +198,12 @@ class FeatureTrs:
         return [tr.name for tr in self._feature_trs]
 
     def report(self) -> str:
-
+        max_l = max(len(tr.name) for tr in self._feature_trs) + 3
         return "Feature Transforms:\n" + "\n".join(
-            [key_val_fmt(tr.name, tr.output_size) for tr in self._feature_trs]
+            [
+                key_val_fmt(tr.name, tr.output_size, key_len=max_l)
+                for tr in self._feature_trs
+            ]
         )
 
     def __call__(self, flow: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
@@ -131,5 +226,15 @@ def get_feature_tr(feature_name: str) -> _TR:
             return UDPackets("up")
         case "down_packets":
             return UDPackets("down")
+        case "iats":
+            return IAT("any")
+        case "up_iats":
+            return IAT("up")
+        case "down_iats":
+            return IAT("down")
+        case "times_normalized":
+            return Normalize(assets.TIME)
+        case "iats_normalized":
+            return Compose(IAT("any"), Normalize("iat"))
         case _:
             raise ValueError(f"Unknown feature name: {feature_name}")
--- kipl_ml/logging/utils.py
+++ kipl_ml/logging/utils.py
@@ -3,5 +3,5 @@ from typing import Any
 KEY_LEN = 25
 
 
-def key_val_fmt(key: str, val: Any) -> str:
-    return f"{key:>{KEY_LEN}}: {val}"
+def key_val_fmt(key: str, val: Any, key_len: int = KEY_LEN) -> str:
+    return f"{key:>{key_len}}: {val}"
