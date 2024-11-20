91a5abb8670e4eac3e0097792d5590381f1c7e1b
topiko <topiko1987@gmail.com>
2024-11-20 11:47:46 +0100

    Massive refactor in prep pipes. Test go through.

--- kipl_ml/trace/tests.py
+++ kipl_ml/trace/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
+import kipl_ml.data.assets as assets
 import torch
-from kipl_ml.data.assets import assets
 from kipl_ml.trace.features import get_feature_tr
 from kipl_ml.trace.transforms import _TR
 
@@ -17,7 +17,7 @@ class TestTR(unittest.TestCase):
         times = torch.arange(n_packets, dtype=torch.float32)
         sizes = torch.ones_like(times)
 
-        return {assets.DIR: dirs, assets.SIZE: sizes, assets.TIME: times}
+        return {assets.DIRS: dirs, assets.SIZES: sizes, assets.TIMES: times}
 
     def _get_key(self, key: str, trace: dict[str, torch.Tensor]) -> _TR:
         tr = get_feature_tr(key, self.N_PACKETS)
@@ -25,9 +25,9 @@ class TestTR(unittest.TestCase):
         return tr
 
     def _shapes_test(self, tr: _TR, trace: dict[str, torch.Tensor]):
-        # self.assertEqual(tr.input_size, self.N_PACKETS)
-        self.assertEqual(tr.output_size, self.N_PACKETS)
-        self.assertEqual(tr(trace).shape[0], self.N_PACKETS)
+        for k, v in tr.output_sizes.items():
+            self.assertEqual(v, self.N_PACKETS)
+            self.assertEqual(tr(trace)[k].shape[0], self.N_PACKETS)
 
     def _simple_test(self, key: str):
         trace = self._get_trace(self.N_PACKETS)
@@ -35,96 +35,99 @@ class TestTR(unittest.TestCase):
 
         self._shapes_test(tr, trace)
 
-        asset_key = {"dirs": assets.DIR, "sizes": assets.SIZE, "times": assets.TIME}[
-            key
-        ]
-
-        self.assertTrue(torch.allclose(tr(trace), trace[asset_key][: self.N_PACKETS]))
+        for _, v in tr(trace).items():
+            self.assertTrue(torch.allclose(v, trace[key][: self.N_PACKETS]))
 
     def test_dirs(self):
-        self._simple_test("dirs")
+        self._simple_test(assets.DIRS)
 
     def test_sizes(self):
-        self._simple_test("sizes")
+        self._simple_test(assets.SIZES)
 
     def test_times(self):
-        self._simple_test("times")
+        self._simple_test(assets.TIMES)
 
     def test_iats(self):
         trace = self._get_trace(self.N_PACKETS)
-        tr = self._get_key("iats", trace)
+        tr = self._get_key(assets.IATS, trace)
 
         self._shapes_test(tr, trace)
 
-        iats = torch.zeros_like(trace[assets.TIME])
-        iats[1:] = torch.diff(trace[assets.TIME], dim=0)
+        iats = torch.zeros_like(trace[assets.TIMES])
+        iats[1:] = torch.diff(trace[assets.TIMES], dim=0)
 
-        self.assertTrue(torch.allclose(tr(trace), iats))
+        self.assertTrue(torch.allclose(tr(trace)[assets.IATS], iats[: self.N_PACKETS]))
 
     def test_ud_packets(self):
         trace = self._get_trace(self.N_PACKETS)
-        tr_up = self._get_key("up_packets", trace)
-        tr_down = self._get_key("down_packets", trace)
+        tr_up = self._get_key(assets.UP_PACKETS, trace)
+        tr_down = self._get_key(assets.DOWN_PACKETS, trace)
 
         self._shapes_test(tr_up, trace)
         self._shapes_test(tr_down, trace)
 
         self.assertTrue(
             torch.allclose(
-                tr_up(trace), (trace[assets.DIR][: self.N_PACKETS] == 1).float()
+                tr_up(trace)[assets.UP_PACKETS],
+                (trace[assets.DIRS][: self.N_PACKETS] == 1).float(),
             )
         )
         self.assertTrue(
             torch.allclose(
-                tr_down(trace), (trace[assets.DIR][: self.N_PACKETS] == -1).float()
+                tr_down(trace)[assets.DOWN_PACKETS],
+                (trace[assets.DIRS][: self.N_PACKETS] == -1).float(),
             )
         )
 
     def test_ud_iats(self):
         trace = self._get_trace(self.N_PACKETS)
-        tr_up = self._get_key("up_iats", trace)
-        tr_down = self._get_key("down_iats", trace)
+        tr_up = self._get_key(assets.UP_IATS, trace)
+        tr_down = self._get_key(assets.DOWN_IATS, trace)
 
         self._shapes_test(tr_up, trace)
         self._shapes_test(tr_down, trace)
 
-        mask = trace[assets.DIR] == 1
+        mask = trace[assets.DIRS] == 1
         idxs = torch.where(mask)[0]
-        iats = torch.zeros_like(trace[assets.TIME])
-        iats[idxs[1:]] = torch.diff(trace[assets.TIME][mask], dim=0)
+        iats = torch.zeros_like(trace[assets.TIMES])
+        iats[idxs[1:]] = torch.diff(trace[assets.TIMES][mask], dim=0)
+        iats = iats[: self.N_PACKETS]
 
-        self.assertTrue(torch.allclose(tr_up(trace), iats))
+        self.assertTrue(torch.allclose(tr_up(trace)[assets.UP_IATS], iats))
 
-        mask = trace[assets.DIR] == -1
+        mask = trace[assets.DIRS] == -1
         idxs = torch.where(mask)[0]
-        iats = torch.zeros_like(trace[assets.TIME])
-        iats[idxs[1:]] = torch.diff(trace[assets.TIME][mask], dim=0)
+        iats = torch.zeros_like(trace[assets.TIMES])
+        iats[idxs[1:]] = torch.diff(trace[assets.TIMES][mask], dim=0)
+        iats = iats[: self.N_PACKETS]
 
-        self.assertTrue(torch.allclose(tr_down(trace), iats))
+        self.assertTrue(torch.allclose(tr_down(trace)[assets.DOWN_IATS], iats))
 
     def test_normalize_iats(self):
         trace = self._get_trace(self.N_PACKETS)
-        tr = self._get_key("iats_normalized", trace)
+        tr = self._get_key(assets.IATS_NORMALIZED, trace)
 
         self._shapes_test(tr, trace)
 
-        iats = torch.zeros_like(trace[assets.TIME])
-        iats[1:] = torch.diff(trace[assets.TIME], dim=0)
+        iats = torch.zeros_like(trace[assets.TIMES])[: self.N_PACKETS]
+        iats[1:] = torch.diff(trace[assets.TIMES][: self.N_PACKETS], dim=0)
 
         iats -= iats.mean()
         iats /= iats.std()
 
-        self.assertTrue(torch.allclose(tr(trace), iats))
+        self.assertTrue(torch.allclose(tr(trace)[assets.IATS_NORMALIZED], iats))
 
     def test_normalized_times(self):
         trace = self._get_trace(self.N_PACKETS)
-        tr = self._get_key("times_normalized", trace)
+        tr = self._get_key(assets.TIMES_NORMALIZED, trace)
 
         self._shapes_test(tr, trace)
 
-        times = trace[assets.TIME]
+        times = trace[assets.TIMES][: self.N_PACKETS]
         times -= times.mean()
         times /= times.std()
+
+        self.assertTrue(torch.allclose(tr(trace)[assets.TIMES_NORMALIZED], times))
 
 
 if __name__ == "__main__":
