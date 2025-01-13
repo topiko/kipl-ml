dfe05949fa385d55d99479713c78af2e17a52e68
topiko <topiko1987@gmail.com>
2025-01-13 12:07:42 +0100

    f string.

--- experiment/laserbeak/main.py
+++ experiment/laserbeak/main.py
@@ -61,7 +61,7 @@ def main(cfg: DictConfig):
 
     if model_config.get("input_size"):
         logger.warning(
-            "Trace len {n_packets} overridden my model config -> {model_config['input_size']}"
+            f"Trace len {n_packets} overwritten my model config -> {model_config['input_size']}"
         )
 
         n_packets = model_config["input_size"]
