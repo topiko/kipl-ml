b374587598bdb0cbc19d15ce80c0b06724d0b3c0
topiko <topiko1987@gmail.com>
2025-01-13 14:19:48 +0100

    Import laserbeak configs.

--- kipl_ml/models/utils.py
+++ kipl_ml/models/utils.py
@@ -1,3 +1,16 @@
+import json
+import os
+
+import configs as lasereak_configs
+
+
 def count_parameters(model) -> str:
     np = sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6
     return f"{np:.02f} M"
+
+
+def get_laserbeak_model_config(model_name: str) -> dict:
+    config_path = os.path.join(list(lasereak_configs.__path__)[0], model_name + ".json")
+    with open(config_path, "r") as fi:
+        model_config = json.load(fi)
+    return model_config
