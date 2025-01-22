92b51013040ad83565a65123f6cd8caeed928873
topiko <topiko1987@gmail.com>
2025-01-22 13:35:49 +0100

    Minor setup mod.

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
+
+# Function to parse requirements.txt
+def parse_requirements(filename: str):
+    with open(filename, "r") as file:
+        return [
+            line.strip() for line in file if line.strip() and not line.startswith("#")
+        ]
+
+
 setup(
     name="kipl-ml",
     version="0.0",
@@ -13,5 +22,7 @@ setup(
     author_email="topiko1987@gmail.com",
     url="http://www.tbs/",
     packages=["kipl_ml"],  # same as name
-    install_requires=[],  # external packages as dependencies
+    install_requires=parse_requirements(
+        "requirements.txt"
+    ),  # external packages as dependencies
 )
