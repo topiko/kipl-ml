a99d73e7c1f00a598cf317425eeada2370cc7b76
topiko <topiko1987@gmail.com>
2025-02-10 12:45:06 +0100

    Tweak.

--- experiment/ephemeral_defences/script.sh
+++ experiment/ephemeral_defences/script.sh
@@ -5,7 +5,7 @@
 for defence_aug in 1 2 4 8 16 0
 do
 	uv run python main.py --config-name=config defence=no_defence dataset.defence_augmentation=$defence_aug
-	for nmachines in 1 10 100 1000 5000 15000
+	for nmachines in 1 10 100 1000 10000
 	do
 		echo "Running with $nmachines machines"
 		uv run python main.py --config-name=config \
