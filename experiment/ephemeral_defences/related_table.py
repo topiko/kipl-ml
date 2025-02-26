aaed3f0f8369ab40f04fe4d13a772da34f070238
topiko <topiko1987@gmail.com>
2025-02-26 10:51:04 +0100

    Related work tables.

---
+++ experiment/ephemeral_defences/related_table.py
@@ -0,0 +1,83 @@
+import argparse
+import unicodedata
+
+import pandas as pd
+
+from kipl_ml.tools.mlflow_utils import list_runs
+
+
+def _parse_df(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+
+    groupby = [
+        "params.dataset_name",
+        "params.model_name",
+        "params.defence.defence-type",
+        "params.network_state",
+    ]
+
+    metric = f"metrics.{metric}"
+    infty = unicodedata.lookup("Rocket")
+    bottleneck = unicodedata.lookup("Hourglass")
+    res = (
+        df.groupby(groupby)
+        .apply(
+            lambda x: f"{x.loc[:, metric].mean():.3f}\u00b1{x.loc[:, metric].std():.3f}",
+            include_groups=False,
+        )
+        .to_frame()
+        .reset_index()
+        .pivot(
+            index=["params.defence.defence-type", "params.network_state"],
+            columns=["params.dataset_name", "params.model_name"],
+            values=0,
+        )
+        .rename_axis(index=["", ""], columns=["", ""])
+        .sort_index(axis=1)
+        .rename(
+            index={"infinite": infty, "bottleneck": bottleneck},
+            level=1,
+        )
+    )
+
+    if metric.startswith("metrics.def."):
+        res = res.loc[:, (slice(None), "df")].rename(
+            columns={"df": metric.replace("metrics.def.", "")}, level=1
+        )
+    elif metric == "metrics.test_accuracy":
+        res = res.rename(columns=lambda x: f"acc-{x}", level=1)
+
+    return res
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-en",
+        "--experiment-name",
+        type=str,
+        nargs="+",
+        required=True,
+        help="Which experiment(s) to consider",
+    )
+
+    args = parser.parse_args()
+
+    df = list_runs(args.experiment_name)
+    df = df[~df.loc[:, "params.test_xv"].isnull()]
+
+    res_acc = _parse_df(df, "test_accuracy")
+    res_bw = _parse_df(df, "def.bandwidth")
+    res_delay = _parse_df(df, "def.delay")
+
+    res = pd.concat((res_acc, res_bw, res_delay), axis=1)
+
+    print(res)
+    with open(f"tables/{args.experiment_name}_table.txt", "w", encoding="utf-8") as f:
+        for line in str(res).split("\n"):
+            f.write(line + "\n")
+
+    res.to_latex(f"tables/{args.experiment_name}_table.tex")
+
+
+if __name__ == "__main__":
+    main()
--- experiment/ephemeral_defences/related_work.sh
+++ experiment/ephemeral_defences/related_work.sh
@@ -6,7 +6,7 @@ defaug=1
 
 for netwk_state in infinite bottleneck
 do
-	for defence in no_defence "front-$netwk_state" interspace # breakpad "tamaraw-$network_state"
+	for defence in no_defence "front-$netwk_state" interspace breakpad "tamaraw-$netwk_state"
 	do
 		expr_name="Ephemeral-Related-Aug$defaug-$netwk_state"
 		common="misc.defence_augmentation=$defaug train.n_epochs=$nepochs mlflow.experiment_name=$expr_name network=$netwk_state"
@@ -14,10 +14,9 @@ do
 
 		uv run python main.py --config-name=df defence=$defence $common
 		uv run python main.py --config-name=rf defence=$defence $common
-		# uv run python main.py --config-name=df-multi defence=$defence $common_lb
+		uv run python main.py --config-name=df-multi defence=$defence $common_lb
 		# uv run python main.py --config-name=laserbeak defence=$defence $common_lb
 
-		uv run python xv_table.py -en $expr_name
 	done
 done
 
