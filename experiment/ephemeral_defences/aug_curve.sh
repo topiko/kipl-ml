bc365f749244c7fee1b3494a19152d8246444df2
topiko <topiko1987@gmail.com>
2025-03-20 15:07:09 +0100

    Restore 5 fold xv splits.

--- experiment/ephemeral_defences/aug_curve.sh
+++ experiment/ephemeral_defences/aug_curve.sh
@@ -9,15 +9,15 @@ for model in df df-multi rf
 do
 	for fixed_per_trace in False True
 	do
-		if [ "$bool_" = true ]; then
+		if [ "$fixed_per_trace" = True ]; then
 			fixing_="fixed_per_trace"
 		else
 			fixing_="no_fixing"
 		fi
 
-		for aug in 1 2 4 8 16 32 0
+		for aug in 0 1 2 4 8 16 32
 		do
-			common="misc.defence_augmentation=$aug lr_scheduler=plateau train.n_epochs=$nepochs network=$ntwork"
+			common="train.defence_augmentation=$aug lr_scheduler=plateau train.n_epochs=$nepochs network=$ntwork"
 
 			uv run python main.py --config-name=$model defence=no_defence $common  misc.mlflow.experiment_name="$expr_name-no_fixing" misc.ignore_existing=False
 
