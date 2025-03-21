4f76d8638da737d25ff5611446c77326710f8abe
topiko <topiko1987@gmail.com>
2025-03-21 09:03:55 +0100

    Make the patience reflect the augmentation.

--- experiment/ephemeral_defences/aug_curve.sh
+++ experiment/ephemeral_defences/aug_curve.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-nepochs=30
-expr_name="Ephemeral-Aug-Curve"
 ntwork="infinite"
+expr_name="Ephemeral-Aug-Curve-$ntwork"
+
+div() {
+  if [[ $2 -eq 0 ]]; then
+    echo "$1"
+  else
+    echo $(( $1 / $2 ))
+  fi
+}
 
 
 for model in df df-multi rf
@@ -17,11 +24,12 @@ do
 
 		for aug in 0 1 2 4 8 16 32
 		do
-			common="train.defence_augmentation=$aug lr_scheduler=plateau train.n_epochs=$nepochs network=$ntwork"
+			patience=$(div 32 $aug)
+			common="train.defence_augmentation=$aug lr_scheduler=plateau network=$ntwork train.patience=$patience train.test_splits=[1,2]"
 
 			uv run python main.py --config-name=$model defence=no_defence $common  misc.mlflow.experiment_name="$expr_name-no_fixing" misc.ignore_existing=False
 
-			for defence in "front-$ntwork" # "ephemeral-$ntwork"
+			for defence in "front-$ntwork" #
 			do
 				front_ephemeral="$common defence.fixed_per_trace=$fixed_per_trace misc.mlflow.experiment_name=$expr_name-$fixing_"
 				uv run python main.py --config-name=$model defence=$defence $front_ephemeral
