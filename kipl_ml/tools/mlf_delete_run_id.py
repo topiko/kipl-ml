983ffa83fb54cc69c08ec7a6ef9b699f7be3906b
topiko <topiko1987@gmail.com>
2025-03-24 19:28:16 +0100

    Remove also experiments.

--- kipl_ml/tools/mlf_delete_run_id.py
+++ kipl_ml/tools/mlf_delete_run_id.py
@@ -1,16 +1,20 @@
 import argparse
 
 import mlflow
+
 from kipl_ml.tools.mlflow_utils import list_runs
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--run_id", type=str, required=False, help="Run id to delete.")
+    parser.add_argument("--run-id", type=str, required=False, help="Run id to delete.")
+    parser.add_argument(
+        "--experiment-id", type=str, required=False, help="Experiment id to delete."
+    )
 
     args = parser.parse_args()
 
-    if args.run_id is None:
+    if (args.run_id is None) and (args.experiment_id is None):
         runs = list_runs(only_finished=False)
         print(
             runs.loc[
@@ -26,14 +30,28 @@ def main():
         )
         return
 
+    def _delete_run(run_id: str):
+        yn = input(f"Run with id '{run_id}' is about to be deleted. Continue? (y/n)")
+        if yn.lower() == "y":
+            mlflow.delete_run(run_id)
+            print(f"Run with id {args.run_id} was deleted.")
 
-    yn = input(f"Run with id '{args.run_id}' is about to be deleted. Continue? (y/n)")
-    if yn.lower() == "y":
-        mlflow.delete_run(args.run_id)
-        print(f"Run with id {args.run_id} was deleted.")
+        run = mlflow.get_run(run_id)
+        print("Lifecycle:", run.info.lifecycle_stage)
 
-    run = mlflow.get_run(args.run_id)
-    print("Lifecycle:", run.info.lifecycle_stage)
+    def _delete_experiment(experiment_id: str):
+        yn = input(
+            f"Experiment with id '{experiment_id}' is about to be deleted. Continue? (y/n)"
+        )
+        if yn.lower() == "y":
+            mlflow.delete_experiment(experiment_id)
+            print(f"Run with id {args.run_id} was deleted.")
+
+    if args.experiment_id is not None:
+        _delete_experiment(args.experiment_id)
+
+    if args.run_id is not None:
+        _delete_run(args.run_id)
 
 
 if __name__ == "__main__":
