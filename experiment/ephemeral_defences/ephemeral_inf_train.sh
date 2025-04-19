aea55ff5185e8cb509c60a1d6838aaf1be152ca4
topiko <topiko1987@gmail.com>
2025-04-19 21:09:33 +0200

    Only few xv splits at a time.

--- experiment/ephemeral_defences/ephemeral_inf_train.sh
+++ experiment/ephemeral_defences/ephemeral_inf_train.sh
@@ -8,33 +8,37 @@ fixed_per_trace=false
 source defence_lists.sh
 
 
-for netwk_state in infinite bottleneck
+for xv in "[0,1]" "[2]" "[3]" "[4]"
 do
-
-
-	if [[ $netwk_state == "infinite" ]]; then
-		list_="defences_inf"
-	elif [[ $netwk_state == "bottleneck" ]]; then
-		list_="defences_bottle"
-	fi
-
-	eval "lst=(\"\${${list_}[@]}\")"
-
-
-	for defence in "${lst[@]}"
+	for netwk_state in infinite bottleneck
 	do
 
-		echo $defence
-		expr_name="EphemeralOFFICIAL-inftrain-$netwk_state"
+
+		if [[ $netwk_state == "infinite" ]]; then
+			list_="defences_inf"
+		elif [[ $netwk_state == "bottleneck" ]]; then
+			list_="defences_bottle"
+		fi
+
+		eval "lst=(\"\${${list_}[@]}\")"
 
 
-		common="train.defence_augmentation=$defaug lr_scheduler=plateau lr_scheduler.lr_patience=$lr_patience misc.mlflow.experiment_name=$expr_name network=$netwk_state train.patience=$patience train.n_epochs=0"
+		for defence in "${lst[@]}"
+		do
 
-		uv run python main.py --config-name=df defence=$defence $common
-		uv run python main.py --config-name=rf defence=$defence $common
-		uv run python main.py --config-name=df-multi defence=$defence $common
-		# uv run python main.py --config-name=laserbeak defence=$defence $common
+			echo $defence
+			expr_name="EphemeralMORE-OFFICIAL-inftrain-$netwk_state"
 
+
+			common="train.defence_augmentation=$defaug lr_scheduler=plateau lr_scheduler.lr_patience=$lr_patience misc.mlflow.experiment_name=$expr_name network=$netwk_state train.patience=$patience train.n_epochs=0 dataset.test_splits=$xv"
+
+			uv run python main.py --config-name=df defence=$defence $common
+			uv run python main.py --config-name=rf defence=$defence $common
+			uv run python main.py --config-name=df-multi defence=$defence $common
+			# uv run python main.py --config-name=rf_star defence=$defence $common
+			# uv run python main.py --config-name=laserbeak defence=$defence $common
+
+		done
 	done
 done
 
