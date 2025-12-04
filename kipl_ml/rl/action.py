6f38a2c225765f0fc4e3e6e6831216358f097d84
topiko <topiko1987@gmail.com>
2025-12-04 13:01:19 +0200

    Phrasing correction in logging.

--- kipl_ml/rl/action.py
+++ kipl_ml/rl/action.py
@@ -34,7 +34,7 @@ class DelayActionExec:
         self._counts: torch.Tensor
         self.dt: float = dt
         self.t: float = 0.0
-        logger.warning("DelayActionExec is dummy implementation.")
+        logger.warning("DelayActionExec has dummy implementation.")
 
     def step(self, curXobs: dict[Feats, torch.Tensor]):
         self._feature_dict: dict[Feats, torch.Tensor] = curXobs
