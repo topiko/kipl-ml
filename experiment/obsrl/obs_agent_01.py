4fabd04594e5306ec973d5607c9a01c53c391550
topiko <topiko1987@gmail.com>
2025-12-04 16:08:49 +0200

    Bug fixes in gae...

--- experiment/obsrl/obs_agent_01.py
+++ experiment/obsrl/obs_agent_01.py
@@ -38,35 +38,42 @@ DATASET = Datasets.BIGENOUGH
 
 
 def get_returns(rewards: torch.Tensor, gamma: float) -> torch.Tensor:
+    _, T = rewards.shape
     G = torch.zeros_like(rewards)
     R = 0
-    i = 0
 
-    for r in rewards.flip(dims=(1,)).T:
-        R = r + gamma * R
-        G[:, -1 - i] = R
-        i += 1
+    # for r in rewards.flip(dims=(1,)).T:
+    for t in reversed(range(T)):
+        R = rewards[t] + gamma * R
+        G[:, t] = R
     return G
 
 
 def get_gae(
     rewards: torch.Tensor, values: torch.Tensor, lambda_: float, gamma: float
 ) -> torch.Tensor:
+    """
+    rewards, values: [B, T]
+    returns: GAE advantages of shape [B, T]
+    """
+
+    B, T = rewards.shape
     # delta = r_t + gamma * V_t+1 - V_t
     deltas = torch.zeros_like(rewards)
     deltas[:, :-1] = rewards[:, :-1] + gamma * values[:, 1:] - values[:, :-1]
     # V_t+1 for last times step = 0
-    deltas[-1] = rewards[-1] - values[-1]
+    deltas[:, -1] = rewards[:, -1] - values[:, -1]
+
+    gae = torch.zeros_like(rewards)
+    running = torch.zeros(B, device=rewards.device)
 
     y = lambda_ * gamma
-    GAE = torch.zeros_like(rewards)
-
     gae_ = 0
-    for i, delta in enumerate(reversed(deltas)):
-        gae_ = delta + gae_ * y
-        GAE[-i - 1] = gae_
+    for t in reversed(range(T)):
+        gae_ = deltas[:, t] + running * y
+        gae[:, t] = gae_
 
-    return GAE
+    return gae
 
 
 def _plot_set(
@@ -302,13 +309,14 @@ def main(cfg: DictConfig):
                         )
                     )
 
-                    G = get_returns(rewards, gamma=gamma)
                     if cfg.advantages.type == "mc":
+                        G = get_returns(rewards, gamma=gamma)
                         advantages = G - values
                     elif cfg.advantages.type == "gae":
                         advantages = get_gae(
                             rewards, values, lambda_=cfg.advantages.lambda_, gamma=gamma
                         )
+                        G = advantages + values
                     else:
                         raise NotImplementedError(
                             "Invalid advantage type: {cfg.advantages.type}"
@@ -321,7 +329,7 @@ def main(cfg: DictConfig):
 
                     # Compute losses
                     policy_loss = -(log_ps * advantages.detach()).mean()
-                    value_loss = 0.5 * (values - G).pow(2).sqrt().mean()
+                    value_loss = 0.5 * (values - G).pow(2).mean()
                     entropy_loss = -10 * entropies.mean()
                     hidden_penalty = hidden_penalty * 1
 
