0474e0a3778385aa225ce271798c8ac5114f1014
topiko <topiko1987@gmail.com>
2025-12-06 13:08:56 +0200

    Bug fix in padding labeling.

--- kipl_ml/rl/action.py
+++ kipl_ml/rl/action.py
@@ -18,8 +18,8 @@ def _sort_feature_dict(
 
     dirs = feature_dict[Feats.DIRS].gather(1, indices)
     sorted_times = _flush_left(sorted_times, dirs != 0)
-    dirs = _flush_left(dirs, dirs != 0)
     padding = _flush_left(feature_dict[Feats.PADDING].gather(1, indices), dirs != 0)
+    dirs = _flush_left(dirs, dirs != 0)
 
     feature_dict[Feats.TIMES] = sorted_times
     feature_dict[Feats.DIRS] = dirs
