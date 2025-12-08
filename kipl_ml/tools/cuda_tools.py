1f78cd809e1889b4b814cdab20ced11b9871082e
topiko <topiko1987@gmail.com>
2025-12-08 13:14:45 +0200

    Tensor footprint.

--- kipl_ml/tools/cuda_tools.py
+++ kipl_ml/tools/cuda_tools.py
@@ -1,7 +1,34 @@
 import torch
+
 from kipl_ml.logging.logger import get_logger
 
 logger = get_logger(__name__)
+
+
+def tensor_footprint(t: torch.Tensor, include_grad: bool = True) -> str:
+    """Calculate the memory footprint of a tensor in bytes.
+
+    Args:
+        tensor (torch.Tensor): The tensor to calculate the footprint for.
+        include_grad (bool, optional): Whether to include the gradient tensor's
+            footprint if it exists. Defaults to True.
+
+    Returns:
+        str: The memory footprint in a human-readable format.
+    """
+
+    bytes_ = t.element_size() * t.nelement()
+
+    if include_grad and t.grad is not None:
+        bytes_ += t.grad.element_size() * t.grad.nelement()
+
+    if bytes_ < 1024:
+        return f"{bytes_} B"
+    if bytes_ < 1024**2:
+        return f"{bytes_ / 1024:.2f} KB"
+    if bytes_ < 1024**3:
+        return f"{bytes_ / 1024**2:.2f} MB"
+    return f"{bytes_ / 1024**3:.2f} GB"
 
 
 def get_device() -> torch.DeviceObjType:
