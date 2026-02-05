d0e62588f8a443382fed975114fe2fe4ae046a84
topiko <topiko1987@gmail.com>
2026-02-05 21:24:04 +0200

    Script sweep update.

--- experiment/obsrl/script.sh
+++ experiment/obsrl/script.sh
@@ -7,16 +7,20 @@ for per in 100 200 50
 do
 	for rnn_lr_reduction in  1.0 0.5
 	do
-		for adv in mc mc_w_bootstrap
+		for sep_critic in true false
 		do
-			uv run python obs_agent_01.py \
-				advantages.type=$adv  \
-				h_detach_period=$per \
-				rnn_lr_reduction=$rnn_lr_reduction \
-				max_epochs=100 \
-				league_size=50 \
-				train_subset_frac=0.2 \
-				experiment_name=obsrl_sweep2
+			for adv in mc mc_w_bootstrap
+			do
+				uv run python obs_agent_01.py \
+					advantages.type=$adv  \
+					h_detach_period=$per \
+					rnn_lr_reduction=$rnn_lr_reduction \
+					max_epochs=100 \
+					league_size=50 \
+					train_subset_frac=0.2 \
+					separate_critic=$sep_critic \
+					experiment_name=obsrl_sweep2
+			done
 		done
 	done
 done
