8fb132a64b8dd1743c0e21231d8bc4a8a368252e
topiko <topiko1987@gmail.com>
2026-02-13 16:15:12 +0200

    New expr name.

--- experiment/obsrl/sisysweep.sh
+++ experiment/obsrl/sisysweep.sh
@@ -6,15 +6,18 @@ set -e
 
 adv=mc
 
-
-for reuse in True False
+for send_mode in fixed spread
 do
-	for sep_critic in false true
+	for reuse in True False
 	do
-		uv run python sisyphus.py \
-			advantages.type=$adv  \
-			obs.separate_critic=$sep_critic \
-			obs.reuse_obs_and_critic=$reuse \
-			experiment_name=sisy_sweep
+		for sep_critic in false true
+		do
+			uv run python sisyphus.py \
+				advantages.type=$adv  \
+				obs.separate_critic=$sep_critic \
+				obs.reuse_obs_and_critic=$reuse \
+				obs.send_mode=$send_mode \
+				experiment_name=sisy_sweep_$send_mode
+		done
 	done
 done
