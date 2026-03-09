201bb05e6a0c4ac44cb62c266285c17330375763
topiko <topiko1987@gmail.com>
2026-03-09 20:15:57 +0200

    Delay runs?

--- experiment/obsrl/sisysweep.sh
+++ experiment/obsrl/sisysweep.sh
@@ -1,24 +1,27 @@
-#/bin/bash
+#!/bin/bash
 #
 #
 # Exit if anything fails..
 set -e
 
 adv=mc
-parent_prefix="advanced-smolin" # audacious-imp
+parent_prefix="audacious-imp" #"advanced-smolin" # audacious-imp
 
-for sep_critic in False True; do
-	for reuse in True False; do
-		for i in $(seq 1 100); do
-			parent_name="$parent_prefix|sep-critic_$sep_critic|reuse_$reuse"
-			echo "=== $parent_name ==="
-			echo "=== Push $i/100 ==="
-			uv run python sisyphus.py \
-				mlflow.parent_run_name=$parent_name \
-				advantages.type=$adv  \
-				obs.separate_critic=$sep_critic \
-				obs.reuse_obs_and_critic=$reuse \
-				experiment_name=sisy_sweep
+for enable_delay in True False; do
+	for sep_critic in False True; do
+		for reuse in True False; do
+			for i in $(seq 1 100); do
+				parent_name="$parent_prefix|delay_$enable_delay|sep-critic_$sep_critic|reuse_$reuse"
+				echo "=== $parent_name ==="
+				echo "=== Push $i/100 ==="
+				uv run python sisyphus.py \
+					mlflow.parent_run_name=$parent_name \
+					advantages.type=$adv  \
+					obs.enable_delay=$enable_delay \
+					obs.separate_critic=$sep_critic \
+					obs.reuse_obs_and_critic=$reuse \
+					experiment_name=sisy_sweep
+			done
 		done
 	done
 done
