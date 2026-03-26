e5e1c36679193cb66716c8beea47da4de64a3d7c
topiko <topiko1987@gmail.com>
2026-03-26 10:01:40 +0200

    Cleaning sweep.

--- experiment/obsrl/sisysweep.sh
+++ experiment/obsrl/sisysweep.sh
@@ -4,24 +4,17 @@
 # Exit if anything fails..
 set -e
 
-adv=mc
-trace_len=5000
-parent_prefix="audacious-imp7-$trace_len" #"advanced-smolin" # audacious-imp
+parent_prefix="audacious-imp-08" #"advanced-smolin" # audacious-imp
 
 
-for sep_critic in false true; do
-	for reuse in true false; do
-		for i in $(seq 1 100); do
-			parent_name="$parent_prefix|delay_$enable_delay|sep-critic_$sep_critic|reuse_$reuse"
-			echo "=== $parent_name ==="
-			echo "=== Push $i/100 ==="
-			uv run python sisyphus.py \
-				mlflow.parent_run_name=$parent_name \
-				advantages.type=$adv  \
-				obs.separate_critic=$sep_critic \
-				obs.reuse_obs_and_critic=$reuse \
-				trace.len=$trace_len \
-				experiment_name=sisy_sweep
-		done
+for div_z in true false; do
+	for i in $(seq 1 100); do
+		parent_name="$parent_prefix|divz-$div_z"
+		echo "=== $parent_name ==="
+		echo "=== Push $i/100 ==="
+		uv run python sisyphus.py \
+			mlflow.parent_run_name=$parent_name \
+			advantages.divide_by_Z=$div_z \
+			experiment_name=sisy_sweep
 	done
 done
