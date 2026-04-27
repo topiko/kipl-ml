50669597aa9ff2fff78bc0e1ad32fbca09a6c310
topiko <topiko1987@gmail.com>
2026-04-27 12:46:55 +0300

    Tiny param changes.

--- experiment/obsrl/sisysweep.sh
+++ experiment/obsrl/sisysweep.sh
@@ -4,7 +4,7 @@
 # Exit if anything fails..
 set -e
 
-parent_prefix="MORE-audacious-imp-06" #"advanced-smolin" # audacious-imp
+parent_prefix="MORE-audacious-imp-07" #"advanced-smolin" # audacious-imp
 
 
 for i in $(seq 1 100); do
